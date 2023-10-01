04a0df8c933f50f2b6c69c337442b99c4fef8558
Mr. Vikash <07mvpn@gmail.com>
2023-10-02 02:22:44 +0530

    Bootsrtap Configrating : Not Complete

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-// navbar script 
-    function toggleMenu() {
-        const navLinks = document.getElementById('navLinks');
-        navLinks.style.display === 'flex' ? navLinks.style.display = 'none' : navLinks.style.display = 'flex';
-      }
-
+// alert("Hello, world!");
 
 
