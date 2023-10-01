e66e1d9cae1b20dabe4ab0b007be1d57b7a89b48
Mr. Vikash <07mvpn@gmail.com>
2023-10-01 23:29:15 +0530

    Navbar Created : Logo, Nav:List, with CSS & JS

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// navbar script 
+    function toggleMenu() {
+        const navLinks = document.getElementById('navLinks');
+        navLinks.style.display === 'flex' ? navLinks.style.display = 'none' : navLinks.style.display = 'flex';
+      }
+
+
+
